df26cd4e6ffe4323b0995b16e102a9f9963da01d
Taylenh <theynel3@gmail.com>
2020-01-22 17:52:25 -0600

    updated adding and deleting todo list items

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  uuid from 'uuid/v4';
+import { todoItem } from './types';
+import TodoItem from './components/TodoItem/TodoItem';
+
+
+type items = todoItem[];
+type updater = 'name' | 'description' | 'importance';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, updateItems] = React.useState<items>([]);
+    const [itemName, updateName] = React.useState<string>("");
+    const [itemDescription, updateDescription] = React.useState<string>("");
+    const [itemImportance, updateImportance] = React.useState<number>(0);
+
+    function handleUpdate(event: React.ChangeEvent<HTMLInputElement>, updater: updater): void {
+      switch (updater){
+        case 'name':
+          updateName(event.target.value);
+          break;
+        case 'description':
+          updateDescription(event.target.value);
+          break;
+        case 'importance':
+          updateImportance(parseInt(event.target.value));
+          break;
+      }
+    }
+
+    function handleAdd(event: React.MouseEvent ): void {
+      event.preventDefault();
+      let item: todoItem = {
+        name : itemName,
+        description: itemDescription,
+        importance: itemImportance,
+        id: uuid()
+      }
+      updateItems([...items, item]);
+    }
+
+    function handleDelete(id: string): void{
+      updateItems(items.filter(item => item.id !== id));
+    }
+
+    return (
+      <>
+        <h1>Todo List</h1>
+        <form>
+          <input type='text' value={itemName} onChange={(e) => handleUpdate(e, 'name')} placeholder='Todo name' />
+          <input type='textarea' value={itemDescription} onChange={(e) => handleUpdate(e, 'description')} placeholder='Todo description' />
+          Item Importance: <input type='number' value={itemImportance} onChange={(e) => handleUpdate(e, 'importance')} />
+          <button value='Add to list' onClick={(e) => handleAdd(e)} >Add to list</button>
+        </form>
+        <hr/>
+        {items.map((item) => {
+          return <TodoItem todoItem={item} handleDelete={handleDelete} key={item.id} />
+        })}
+      </>
+    );
 }
 
 export default App;
---
+++ src/types.tsx
@@ -0,0 +1,7 @@
+export interface todoItem {
+    name: string;
+    description: string;
+    importance: number;
+    id: string;
+};
+
